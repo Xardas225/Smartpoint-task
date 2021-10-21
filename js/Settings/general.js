a378846aba09417a1700c2dfac24b1b86bc93ed8
Alexandr <alexanderderevkov@yandex.ru>
2021-10-21 23:19:01 +0300

    intermediate commit

--- js/Settings/general.js
+++ js/Settings/general.js
@@ -49,152 +49,164 @@ $('document').ready(function () {
         };
     };
 
-    // Формирование объекта экспорта данных - страница Общие
-    const generalForm = document.querySelector('#generalForm');
+    // // Формирование объекта экспорта данных - страница Общие
+    // const generalForm = document.querySelector('#generalForm');
 
-    $("#generalForm").validate({
-        submitHandler: function (form, event) {
+    // $("#generalForm").validate({
+    //     submitHandler: function (form, event) {
 
-            event.preventDefault();
+    //         event.preventDefault();
 
-            let name = event.target.name.value
-            timezone = event.target.timezone.value
-            key = event.target.key.value
-            phones = event.target.phones.value.split(',')
-            let data = {
-                "name": name,
-                "timezone": timezone,
-                "key": key,
-                "phones": phones,
-                "Working Time": {
-                    "0": [
-                        {
-                            "from:": event.target.from[0].value,
-                            "to": event.target.to[0].value
-                        }
-                    ],
-                    "1": [
-                        {
-                            "from:": event.target.from[1].value,
-                            "to": event.target.to[1].value
-                        }
-                    ],
-                    "2": [
-                        {
-                            "from:": event.target.from[2].value,
-                            "to": event.target.to[2].value
-                        }
-                    ],
-                    "3": [
-                        {
-                            "from:": event.target.from[3].value,
-                            "to": event.target.to[3].value
-                        }
-                    ],
-                    "4": [
-                        {
-                            "from:": event.target.from[4].value,
-                            "to": event.target.to[4].value
-                        }
-                    ],
-                    "5": [
-                        {
-                            "from:": event.target.from[5].value,
-                            "to": event.target.to[5].value
-                        }
-                    ],
-                    "6": [
-                        {
-                            "from:": event.target.from[6].value,
-                            "to": event.target.to[6].value
-                        }
-                    ],
-                }
+    //         let name = event.target.name.value
+    //         timezone = event.target.timezone.value
+    //         key = event.target.key.value
+    //         phones = event.target.phones.value.split(',')
+    //         let data = {
+    //             "name": name,
+    //             "timezone": timezone,
+    //             "key": key,
+    //             "phones": phones,
+    //             "Working Time": {
+    //                 "0": [
+    //                     {
+    //                         "from:": event.target.from[0].value,
+    //                         "to": event.target.to[0].value
+    //                     }
+    //                 ],
+    //                 "1": [
+    //                     {
+    //                         "from:": event.target.from[1].value,
+    //                         "to": event.target.to[1].value
+    //                     }
+    //                 ],
+    //                 "2": [
+    //                     {
+    //                         "from:": event.target.from[2].value,
+    //                         "to": event.target.to[2].value
+    //                     }
+    //                 ],
+    //                 "3": [
+    //                     {
+    //                         "from:": event.target.from[3].value,
+    //                         "to": event.target.to[3].value
+    //                     }
+    //                 ],
+    //                 "4": [
+    //                     {
+    //                         "from:": event.target.from[4].value,
+    //                         "to": event.target.to[4].value
+    //                     }
+    //                 ],
+    //                 "5": [
+    //                     {
+    //                         "from:": event.target.from[5].value,
+    //                         "to": event.target.to[5].value
+    //                     }
+    //                 ],
+    //                 "6": [
+    //                     {
+    //                         "from:": event.target.from[6].value,
+    //                         "to": event.target.to[6].value
+    //                     }
+    //                 ],
+    //             }
 
-            };
-            console.log(JSON.stringify(data))
-        },
-        rules: {
-            name: {
-                required: true,
-                minlength: 3
-            },
-            timezone: {
-                required: true
-            },
-            key: {
-                required: true,
-                minlength: true
-            },
-            phones: {
-                required: true,
-            },
-        },
-        ignore: [],
-        messages: {
-            name: {
-                required: "Введите имя"
-            },
-            timezone: {
-                required: "Выберите часовой пояс"
-            },
-            key: {
-                required: "Сгенерируйте API-ключ",
-                minlength: "Недостаточно символов для создания API-ключа"
-            },
-            phones: {
-                required: "Введите номер телефона"
-            }
-        }
-    });
-
-
+    //         };
+    //         console.log(JSON.stringify(data))
+    //     },
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 3
+    //         },
+    //         timezone: {
+    //             required: true
+    //         },
+    //         key: {
+    //             required: true,
+    //             minlength: true
+    //         },
+    //         phones: {
+    //             required: true,
+    //         },
+    //     },
+    //     ignore: [],
+    //     messages: {
+    //         name: {
+    //             required: "Введите имя"
+    //         },
+    //         timezone: {
+    //             required: "Выберите часовой пояс"
+    //         },
+    //         key: {
+    //             required: "Сгенерируйте API-ключ",
+    //             minlength: "Недостаточно символов для создания API-ключа"
+    //         },
+    //         phones: {
+    //             required: "Введите номер телефона"
+    //         }
+    //     }
+    // });
 
 })
 
 
-// data.map(elem => { 
-//     let { element, elementClass, type, name, label, attribute } = elem
-//     return elem
-// })
-
-let cardSystem = document.querySelector('.card-system__content')
 
 
 let elementConfig = [
     {
         'element': 'input',
-        'elementClass': 'form-control',
+        'elementClass': ['form-control', 'name'],
         'type': 'text',
         'name': 'name',
         'label': 'Название',
     },
     {
         'element': 'select',
-        'elementClass': 'form-select',
+        'elementClass': ['form-select', 'timezone'],
         'name': 'timezone',
         'label': 'Часовой пояс',
+        'attribute': ['aria-label', 'timezone'],
         'options': [
-            ['Москва', 'Moscow', false, false]
+            ['Москва', 'Moscow'],
+            ['Волгоград  ', 'Volgograd  '],
+            ['Екатеринбург', 'Ekaterinburg'],
+            ['Пермь', 'Perm'],
         ]
     },
     {
         'element': 'input',
-        'elementClass': 'form-control',
+        'elementClass': ['form-control', 'key'],
         'type': 'text',
         'name': 'key',
         'label': 'Ключ АПИ',
-        'attribute': 'disabled'
+        'attribute': ['disabled', true],
+    },
+    {
+        'element': 'button',
+        'elementClass': ['btn', 'btn-active'],
+        'name': 'active',
+        'attribute': ['src', './images/unlock.svg']
+    },
+    {
+        'element': 'button',
+        'elementClass': ['btn', 'btn-random'],
+        'name': 'random',
+        'attribute': ['src', './images/unlock.svg']
     },
     {
         'element': 'input',
-        'elementClass': 'form-control',
+        'elementClass': ['form-control', 'phones'],
         'type': 'text',
         'name': 'phones',
-        'label': 'Телефоны'
+        'label': 'Телефоны',
+        'attribute': ['data-role', 'tagsinput']
     },
 
+
 ]
+
+
 
 class FormBlock {
 
@@ -206,7 +218,8 @@ class FormBlock {
      * _value - value of element
      * _label - create label 
      * _attribute - add attribute
-     * appendToDocument - method which the append FormDblock in Document
+     * appendFormToDocument - method which the append Form in Document
+     * appendFormGroupToDocument - method which the append Form Group in Document
      * returnForm - method which return FormBlock
      */
 
@@ -216,6 +229,8 @@ class FormBlock {
     constructor() {
         this._element = document.createElement('input')
         this._type = 'text'
+        this._elementClass = []
+        this._attribute = []
     }
 
     /**
@@ -245,9 +260,20 @@ class FormBlock {
             return false
         }
 
-        this._element.classList.add(elementClass)
+        this._element.classList.add(...elementClass)
     }
 
+    /**
+     * method set name
+    */
+    set name(name) {
+        this._name = name
+        if (name == '' || name == false || name == null || name == undefined) {
+            return false
+        }
+
+        this._element.name = this._name
+    }
 
     /**
      * method set value
@@ -277,10 +303,10 @@ class FormBlock {
      * method set label
      */
     set label(labelText) {
-        this._label = document.createElement('label')
         if (labelText == '' || labelText == false || labelText == null || labelText == undefined) {
             return false
         } else if (labelText) {
+            this._label = document.createElement('label')
             this._label.innerHTML = labelText
         }
     }
@@ -289,9 +315,9 @@ class FormBlock {
      * method set element
      */
     attribute(attribute) {
-        if(!this._element.hasAttribute(attribute)) {
-            this._element.setAttribute(attribute, true)
-        } 
+        if (!this._element.hasAttribute(attribute) && attribute) {
+            this._element.setAttribute(...attribute)
+        }
 
         return false
     }
@@ -301,7 +327,7 @@ class FormBlock {
      */
     appendFormToDocument(DOMElement) {
         let div = document.createElement('div')
-        div.classList.add('mb-3')
+        div.classList.add('mb-3', `${this._name}`)
 
         if (this._element && this._label) {
             div.append(this._label)
@@ -333,39 +359,87 @@ class FormBlock {
         }
     }
 
+    /**
+     * method appendFormGroupToDocument
+     */
+    appendFormGroupToDocument(data, DOMElement) {
+        for (let i = 0; i < data.length; i++) {
+            let elementData = data[i].name
+
+            switch (data[i].element) {
+                case 'select':
+                    elementData = new SelectBlock();
+                    
+                    elementData.element = data[i].element;
+                    elementData.addedOptions(data[i].options);
+                    elementData.type = data[i].type
+                    elementData.elementClass = data[i].elementClass
+                    elementData.name = data[i].name
+                    elementData.label = data[i].label
+                    elementData.attribute(data[i].attribute)
+
+                    elementData.appendFormToDocument(DOMElement)
+                    break;
+                case 'button':
+                    elementData = new ButtonBlock();
+
+                    elementData.element = data[i].element;
+                    elementData.type = data[i].type
+                    elementData.elementClass = data[i].elementClass
+                    elementData.name = data[i].name
+                    elementData.label = data[i].label
+                    elementData.attribute(data[i].attribute)
+
+                    elementData.appendButtonToElement(DOMElement)
+                    break;
+                default:
+                    elementData = new FormBlock();
+                    elementData.element = data[i].element;
+                    elementData.type = data[i].type
+                    elementData.elementClass = data[i].elementClass
+                    elementData.name = data[i].name
+                    elementData.label = data[i].label
+                    elementData.attribute(data[i].attribute)
+
+                    elementData.appendFormToDocument(DOMElement)
+            }
+
+        }
+
+
+    }
+
 }
+
+
+
 
 class SelectBlock extends FormBlock {
 
-    constructor(...arg) {
-        super(...arg)
-
-    }
-
     addedOptions(options) {
-
+        options.forEach(el => {
+            let option = document.createElement('option')
+            option.text = el[0].trim()
+            option.value = el[1].trim()
+            this._element.appendChild(option)
+        })
     }
 }
 
+class ButtonBlock extends FormBlock {
 
-for (let i = 0; i < elementConfig.length; i++) {
-    let configName = elementConfig[i].name
-
-    configName = new FormBlock()
-
-    configName.element = elementConfig[i].element
-    configName.type = elementConfig[i].type
-    configName.elementClass = elementConfig[i].elementClass
-    configName.name = elementConfig[i].name
-    configName.label = elementConfig[i].label
-
-    if(elementConfig[i].attribute) {
-        configName.attribute(elementConfig[i].attribute)
+    appendButtonToElement(DOMElement) {
+        let div = document.createElement('div')
+        div.classList.add(`${this._name}`)
+        div.append(this._element)
+        DOMElement.append(div)
     }
 
-    configName.appendFormToDocument(cardSystem)
 }
 
+
+let GeneralForm = new FormBlock()
+GeneralForm.appendFormGroupToDocument(elementConfig, document.querySelector('.card-system__content'))
 
 
 
